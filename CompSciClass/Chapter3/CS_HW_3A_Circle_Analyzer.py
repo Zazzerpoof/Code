75cf90493de14527edef854e9879b1dd4da63469
Zazzerpoof <zazzerpoof@gmail.com>
2019-05-31 11:50:47 -0500

    Small Eddit

--- CompSciClass/Chapter3/CS_HW_3A_Circle_Analyzer.py
+++ CompSciClass/Chapter3/CS_HW_3A_Circle_Analyzer.py
@@ -2,7 +2,7 @@
 # Gideon Rediger
 print("Circle Analyzer")
 import math
-def analize_circle():
-    r = input("Radius?  ")
+def analize_circle(r):
     print("\nCircle Info:\nradius = " + r + "\ndiameter = " + str(2*float(r)) + "\ncircumference = " + str(math.pi * 2 * float(r)) + "\narea = " + str(math.pi * float(r) **2))
-analize_circle()
+r = input("Radius?  ")
+analize_circle(r)
