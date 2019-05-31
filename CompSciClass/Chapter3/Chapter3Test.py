752cfc003e666e32a2272e0391bb45d3a860bf1c
Zazzerpoof <zazzerpoof@gmail.com>
2019-05-31 16:53:25 -0500

    Almost done lol

--- CompSciClass/Chapter3/Chapter3Test.py
+++ CompSciClass/Chapter3/Chapter3Test.py
@@ -1,6 +1,6 @@
 import math
 import random
-import datetime
+import time
 from datetime import date
 #The first function is an input validator
 def input_validation(description,input_out_of_range,max,min):
@@ -22,8 +22,8 @@ def input_validation(description,input_out_of_range,max,min):
     return i
 
 def square_root():
-    i = input_validation("This function prints a square root. Enter a number (1 - 9999): ", "Error: Number out of system range. Please input a valid number: ", 9999, 1)
-    print("\nThe square root of " + i + " is " + str(math.sqrt(int(i))))
+    i = input_validation("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nThis function prints a square root. Enter a number (1 - 9999): ", "Error: Number out of system range. Please input a valid number: ", 9999, 1)
+    print("\n\n\nThe square root of " + i + " is " + str(math.sqrt(int(i))))
 
 def test_score():
     c = False
@@ -31,7 +31,7 @@ def test_score():
     #The following loop is an input validator; i didnt use the function because this one is slightly different
     while c == False:
         if counter == 0:
-            i = input("Enter any number of test scores (1 - 100). Seperate each by a space: ")
+            i = input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nEnter any number of test scores (1 - 100). Seperate each by a space: ")
         while len(i) == 0:
             i = input("Error: No input. Please input something: ")
         i = i.split()
@@ -49,14 +49,14 @@ def test_score():
     for x in i:
         t += int(x)
     t /= len(i)
-    print("\nThe test average is " + str(t) + "%")
+    print("\n\n\nThe test average is " + str(t) + "%")
 
 def taxes():
-    i = input_validation("This funtion shows how much your sales tax (7.5%) is for your purchase. Enter the purchase amount (<10,000): ", "Error: input out of range. Please input a valid number: ", 9999, 0)
+    i = input_validation("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nThis funtion shows how much your sales tax (7.5%) is for your purchase. Enter the purchase amount (<10,000): ", "Error: input out of range. Please input a valid number: ", 9999, 0)
     print("The sales tax on $" + i + " is $" + str(float(i) * 0.075) + " and the total cost is $" + str(float(i) + float(i) * 0.075))
 
 def randomness():
-    print("The random number between 1 and 100 is " + str(random.randint(1,100)))
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nThe random number between 1 and 100 is " + str(random.randint(1,100)))
 
 def daydif():
     counter = 0
@@ -64,7 +64,7 @@ def daydif():
     while c == False:
         p = False
         if counter == 0:
-            i = input("This function shows how many days there are between today and a date you enter. Enter the date in this format (mm/dd/yy): ")
+            i = input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nThis function shows how many days there are between today and a date you enter. Enter the date in this format (mm/dd/yy): ")
         i = i.split("/")
         while len(i) != 3:
             i = input("Error: invalid string. Please enter something in the format provided: ")
@@ -88,7 +88,44 @@ def daydif():
         counter += 1
     d1 = date(int(i[2]),int(i[0]),int(i[1]))
     if d1 > date.today():
-        print("The number of days between " + str(d1) + " and today is " + str(d1 - date.today())) 
+        print("\n\n\nThe number of days between " + str(d1) + " and today is " + str(d1 - date.today())) 
     else:
-        print("The number of days between " + str(d1) + " and today is " + str(date.today() - d1)) 
-daydif()
+        print("\n\n\nThe number of days between " + str(d1) + " and today is " + str(date.today() - d1)) 
+stop = False
+trustop = False
+x = time.time()
+while trustop == False:
+    while stop == False:
+
+        i = input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nMy Custom Functions\n1. What's the root?\n2. Average my test scores.\n3. Show me the tax.\n4. Randomness.\n5. How many days?\n6. Exit\n Enter an option: ")
+        while i != "1" and i != "2" and i != "3" and i != "4" and i != "5" and i != "6":
+            i = input("Error: invalid input. Please enter a valid command: ")
+        if i == "1":
+            square_root()
+            input("Press Enter to continue")
+        elif i == "2":
+            test_score()
+            input("Press Enter to continue")
+        elif i == "3":
+            taxes()
+            input("Press Enter to continue")
+        elif i == "4":
+            randomness()
+            input("Press Enter to continue")
+        elif i == "5":
+            daydif()
+            input("Press Enter to continue")
+        else:
+            stop = True
+    i = input("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nAre you sure you want to quit? (y or n)\n")
+    while i != "y" and i != "n":
+        i = input("Error: Please enter a valid command. \nAre you sure you want to quit? (y or n)\n")
+    if i == "y":
+        trustop = True
+    else:
+        stop = False
+y = round(time.time() - x)
+seconds = str(y % 60)
+minutes = str(y // 60)
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nThank you. You used the system for " + minutes + " minutes and " + seconds + " seconds. You will be charged $" + str(y * 0.25) + ".")
+
